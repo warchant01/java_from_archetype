32f4d3a20c28719aa0be43d264e0e13199a8fca6
Michael Gutzwiller <michael@techelevator.com>
2017-11-20 14:19:54 -0500

    Fix for alien weight again

--- m3-w2d1-controllers_exercises_pair_starter/src/main/java/com/techelevator/ssg/controller/AlienWeightCalculator.java
+++ m3-w2d1-controllers_exercises_pair_starter/src/main/java/com/techelevator/ssg/controller/AlienWeightCalculator.java
@@ -2,7 +2,7 @@ package com.techelevator.ssg.controller;
 
 public class AlienWeightCalculator {
 
-	private static double gravities[] = {0.37, 0.90, 1.00, 0.17, 0.38, 2.65, 1.13, 1.09, 1.43};
+	private static double gravities[] = {0.37, 0.90, 1.00, 0.38, 2.65, 1.13, 1.09, 1.43};
 	
 	public static int calculateAlienWeight(int planet, int earthWeight)
 	{
